32a4702c72c98463e10ab33d73d240eb21a91287
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-02-13 22:23:52 +0530

    validation finished

--- Front End/src/Pages/SignIn.jsx
+++ Front End/src/Pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './SignIn.css'
 import {  useNavigate } from "react-router-dom";
+import{EyeInvisibleOutlined,EyeOutlined} from "@ant-design/icons";
 
 export default function SignIn() {
     const Navigate=useNavigate();
@@ -8,6 +9,7 @@ export default function SignIn() {
     const[email,setEmail]= useState('');
     const[password,setPassword]= useState('');
     const[errors,setErrors]= useState([]);
+    const[visible,setVisible]=useState(true);
 
     const handleSubmit=()=>{
       event.preventDefault();
@@ -42,22 +44,28 @@ export default function SignIn() {
   return (
     <div className='signInContainer'>
         <div className='leftPanel'>
-          <form onSubmit={handleSubmit}>
+          
             <h1 className='title'>Hellow Friend</h1>
             <p className='paragraph1'>Please enter your Email and password</p>
-          </form>   
+            
             <button className='secoundButton' onClick={()=>Navigate("/signup")} >Sign Up</button>
            
         </div>
-
+        
         <div className='signInForm'>
             <h1 className='title'>Sign In</h1>
-            <input className='label1' type="email" placeholder='Email' onChange={(e)=>setEmail(e.target.value)  } />
+            <input className='label1' type="email"  placeholder='Email' onChange={(e)=>setEmail(e.target.value)  } />
             {errors.email&&<div className='error'>{errors.email}</div>}
-            <input className='label1' type="password" placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
-             {errors.password&&<div className='error'>{errors.password}</div>}           
+            <input className='label1' type={visible?"text":"password"} placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
+            
+            <div className='see' onClick={()=>setVisible(!visible)}>
+              {visible?<EyeOutlined/>:<EyeInvisibleOutlined/>}
+            </div>
+            {errors.password&&<div className='passwordError'>{errors.password}</div>}  
+                      
             <button className='button' onClick={handleSubmit}>Sign In</button>
         </div>
+        
       
     </div>
   )
--- Front End/src/Pages/SignUp.jsx
+++ Front End/src/Pages/SignUp.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React ,{ useState }from 'react'
 import './SignUp.css'
 import {  useNavigate } from "react-router-dom";
+import{EyeInvisibleOutlined,EyeOutlined} from "@ant-design/icons";
 
 export default function SignUp() {
     const Navigate=useNavigate();
+
+    const[email,setEmail]= useState('');
+    const[password,setPassword]= useState('');
+    const[errors,setErrors]= useState([]);
+    const[visible,setVisible]=useState(false);
+    const[reEntedPasswordVisible,setReEntedPasswordVisible]=useState(false);
+    const[reEntedPassword,setReEntedPassword]= useState('');
+
+    const handleSubmit=()=>{
+      event.preventDefault();
+      const errors=validate();
+      setErrors(errors);
+
+    }
+
+    const validate=()=>{
+      const error={};
+
+      if(!email){
+        error.email="Email is Required";
+      }else if(!/\S+@\S+\.\S+/.test(email)){
+        error.email="Email is not valide";
+      }else{
+        error.email='';
+      }
+
+
+      if(!password){
+        error.password="Password is Required";
+      }else if(password.length<8){
+        error.password="Password is not valide";
+      }else{
+        error.password='';
+      }
+
+
+      if(!reEntedPassword){
+        error.reEnted="Password is Required"
+      }
+      else if(!(reEntedPassword===password)){
+        error.reEnted="The password does not match"
+      }
+      
+      else{
+        error.reEnted=""
+      }
+
+
+
+      return error;
+    }
+
   return (
     <div className='signUpContainer'>
         
@@ -12,21 +65,32 @@ export default function SignUp() {
             <h1 className='title'>Create Account</h1>
 
             <input className='label' type="text" placeholder='Name'/>
-            <input className='label' type="email" placeholder='Email'/>
-            <input className='label' type="password" placeholder='Password'/>
-
+            <input className='label' type="email" placeholder='Email'onChange={(e)=>setEmail(e.target.value)}/>
+            {errors.email&&<div className='error'>{errors.email}</div>}
+            <input className='label'  type={visible?"text":"password"}placeholder='Password' onChange={(e)=>setPassword(e.target.value)}/>
+            <div className='see' onClick={()=>setVisible(!visible)}>
+              {visible?<EyeOutlined/>:<EyeInvisibleOutlined/>}
+            </div>
+            {errors.password&&<div className='passwordError'>{errors.password}</div>} 
+            <div className='reEntedPassworPosition'>
+            <input className='label'  type={reEntedPasswordVisible?"text":"password"}placeholder='Re ented Password' onChange={(e)=>setReEntedPassword(e.target.value)}/>
+            <div className='seeReEnted' onClick={()=>setReEntedPasswordVisible(!reEntedPasswordVisible)}>
+              {reEntedPasswordVisible?<EyeOutlined/>:<EyeInvisibleOutlined/>}
+            </div>
+            {errors.reEnted&&<div className='reEntedPasswordError'>{errors.reEnted}</div>} 
+            </div>
             <div className='radioButtonPart'>
               <label className='radioButton'>
-              <input type="radio" value="option1"  />
+              <input type="radio" value="seller"  />
               Seller
               </label>
 
               <label className='radioButton'>
-              <input type="radio" value="option2"  />
+              <input type="radio" value="buyer"  />
               Buyer
               </label>
             </div>
-            <button className='button'>Sign Up</button>
+            <button className='button' onClick={handleSubmit}>Sign Up</button>
             </div>
         
 
