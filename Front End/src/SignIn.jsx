5d3b2e30c29750b83fdfebbf08d4383315b2a465
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-02-13 08:56:45 +0530

    Sign in page is finished

--- Front End/src/SignIn.jsx
+++ Front End/src/SignIn.jsx
@@ -3,13 +3,15 @@ import './SignIn.css'
 
 export default function SignIn() {
   return (
-    <div>
+    <div className='container'>
         <div className='leftPanel'>
-            <h1>Welcome</h1>
-
+            <h1 className='title'>Welcome</h1>
+            <p className='paragraph'>Please login with your personal information to join us</p>
+            <button className='button'>Sign In</button>
         </div>
+
         <div className='form'>
-            <h1>Create Account</h1>
+            <h1 className='title'>Create Account</h1>
 
             <input className='label' type="text" placeholder='Name'/>
             <input className='label' type="email" placeholder='Email'/>
