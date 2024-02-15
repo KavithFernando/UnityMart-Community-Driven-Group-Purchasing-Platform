2d97f5bdd67afefc48cb15f2d85b8e422efb0c15
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-02-15 22:41:18 +0530

    finished footer

--- Front End/src/components/Footer.jsx
+++ Front End/src/components/Footer.jsx
@@ -1,77 +1,30 @@
 import React from "react";
 import './Footer.css'
-import { Link } from "react-router-dom"; // Assuming you're using React Router for navigation
-
+import { Link } from "react-router-dom"; 
+import{InstagramOutlined,WhatsAppOutlined,FacebookOutlined, GithubOutlined   } from "@ant-design/icons";
 const Footer = () => {
     return (
         <div className="footer">
-           <div className="section_padding">
-             <div className="footer_links">
-                <div className="footer_links_div">
-                    <h4>for business</h4>
-                    <a href="/employeer">
-                        <p>employeer</p>
-                    </a>
-                    <a href="/healthplan">
-                        <p>Health plan</p>
-                    </a>
-                    <a href="/individual">
-                        <p>Individual</p>
-                    </a>
-                </div>
-                <div className="footer_links_div">
-                <h4>Resources</h4>
-                    <a href="/resource">
-                        <p>Resource center</p>
-                    </a>
-                    <a href="/resource">
-                        <p>Testimonials</p>
-                    </a>
-                    <a href="/resource">
-                        <p>Stv</p>
-                    </a>
-                </div>
-                <div className="footer_links_div">
-                <h4>Company</h4>
-                    <a href="/about">
-                        <p>About</p>
-                    </a>
-                    <a href="/press">
-                        <p>Press</p>
-                    </a>
-                    <a href="/career">
-                        <p>Career</p>
-                    </a>
-                    <a href="/contact">
-                        <p>Contact</p>
-                    </a>
-                </div>
-                {/* <div className="footer_links_dev">
-                <h4>coming soon</h4>
-                <div className="social media">
-                    <p><img src = {fb} alt=""/></p>
-                    <p><img src = {twitter} alt=""/></p>
-                    <p><img src = {linkedin} alt=""/></p>
-                    <p><img src = {instergram} alt=""/></p>
-                </div>
-                </div>                           */}
-            </div> 
-            {/* lineol */}
-            <hr></hr>
-            <div className="footer.below">
-                 <div className="footer_copyright">
-                    <p>
-                        @{new Date().getFullYear()}CodeInn.All right reserved.
-                    </p>
-                </div> 
-                 <div className="footer_below_links">
-                    <a href="/terms"><div><p>Terms & conditions</p></div></a>
-                    <a href="/privacy"><div><p>privacy</p></div></a>
-                    <a href="/sequrity"><div><p>sequrity</p></div></a>
-                    <a href="/cookie"><div><p>Cookie Declarations</p></div></a>
-                </div> 
+            <h1 className="footerHeadder"><i>Unity Mart</i></h1>
+            <p className="footerPharagraph">Unity Mart's endeavor is<br/> to provide quality product at lowers prices</p>
+            
+            <div className="developTeam">
+            <h2>Develop team</h2>
+            <br/>
+            <p className="name">Pabasara Ravindaka<br/>Kavith Fernando <br/>Sanjaya Perera <br/>Niketh Randil <br/>Subhavitha Dharmasiri </p>
+            
             </div>
-           </div>
+            <button className="aboutUsButton">About Us</button>
+            <div className="footerIcon"> 
+                <a href="" className="icon">{<InstagramOutlined />}</a>
+                <a href="" className="icon">{<WhatsAppOutlined />}</a>
+                <a href="" className="icon">{<FacebookOutlined />}</a>
+                <a href="" className="icon">{<GithubOutlined />}</a>    
+            
+            </div>
+
+            
+          
         </div>
     );
 };
