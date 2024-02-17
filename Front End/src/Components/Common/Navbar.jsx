0fa5b6043ce1fb0323d885103c68b7e22616904e
KavithFernando <153263149+KavithFernando@users.noreply.github.com>
2024-02-17 20:01:45 +0530

    Final navbar

--- Front End/src/Components/Common/Navbar.jsx
+++ Front End/src/Components/Common/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { IoHome } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
+
 import "./Navbar.css"
 
 export default function Navbar() {
   return (
     <nav>
       <h1>UnityMart</h1>
-      <input className='search' type="text" placeholder='Search UnityMart'/>
+      <div className="searchDiv">
+        <input className='search' type="text" placeholder='Search UnityMart'/>
+        <FaSearch/>
+      </div>
       <ul>
         <li><Link to="/"><IoHome/></Link></li>
         <li><Link to="/signin">Login</Link></li>
