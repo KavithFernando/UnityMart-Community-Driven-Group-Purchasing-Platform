1c96fc7d6ec5b59dcf9f1c6a9717a296e09f5b4e
KavithFernando <153263149+KavithFernando@users.noreply.github.com>
2024-02-18 16:12:30 +0530

    Product card

---
+++ Front End/src/Components/PopularProducts/PopularProduct.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./PopularPanel.css"
+
+export default function PopularProduct({ path }) {
+  return (
+      <div className="pop-product">
+        <Link to={path} className='link-area'>
+          <img src="src\ProductImages\headphones.jpg" alt="Product-Image" />
+          <div className="details">
+            <h4>Sony Wireless Headphones</h4>
+            <div className="progress-bar">Placeholder<br/>Placeholder<br/>Placeholder</div>
+            Rs&nbsp;<span className='price'>1500.00</span>
+            <br />
+            <span className='discount'>Join and Save upto 40%</span>
+          </div>
+        </Link>
+      </div>
+  )
+}
