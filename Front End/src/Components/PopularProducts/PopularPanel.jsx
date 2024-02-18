93d0953b82fe6d2728382eb45f73f2143c7e8be2
KavithFernando <153263149+KavithFernando@users.noreply.github.com>
2024-02-18 16:12:46 +0530

    Auto scrolling panel

---
+++ Front End/src/Components/PopularProducts/PopularPanel.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import "./PopularPanel.css"
+import Carousel from 'react-multi-carousel';
+import PopularProduct from './PopularProduct';
+import 'react-multi-carousel/lib/styles.css';
+
+export default function PopularPanel() {
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <div className='pop-panel'>
+      <Carousel
+        className='carousel'
+        responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+      >
+        <PopularProduct/>
+        <PopularProduct/>
+        <PopularProduct/>
+        <PopularProduct/>
+        <PopularProduct/>
+        <PopularProduct/>
+        <PopularProduct/>
+      </Carousel>
+        
+    </div>
+  )
+}
