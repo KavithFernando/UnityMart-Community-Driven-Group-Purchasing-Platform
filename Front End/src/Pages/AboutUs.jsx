1c8c73e732e225758c38320794a0b761c88a630c
Niketh-Randil <127325580+Niketh-Randil@users.noreply.github.com>
2024-02-20 19:50:08 +0530

    about us page
    About us page done. Needs to do the CSS. Contact Us form will be done by susitha.

---
+++ Front End/src/Pages/AboutUs.jsx
@@ -0,0 +1,46 @@
+// AboutUsPage.js
+import React from 'react';
+import './AboutUs.css';
+
+const AboutUsPage = () => {
+  return (
+    <div className="about-us-page">
+      <h1>About Us</h1>
+      {/* Team Members */}
+      <div className="team-members">
+        <h2>Our Team</h2>
+        <div className="team-member-cards">
+         
+          <div className="team-member-card">
+            <h3>Bla Bla </h3>
+            <p><strong>Role:</strong> CEO</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="team-member-card">
+            <h3>Bla Bla </h3>
+            <p><strong>Role:</strong> CTO</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="team-member-card">
+            <h3>Bla</h3>
+            <p><strong>Role:</strong> CEO</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="team-member-card">
+            <h3>Bla</h3>
+            <p><strong>Role:</strong> CTO</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="team-member-card">
+            <h3>Bla</h3>
+            <p><strong>Role:</strong> CTO</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsPage;
