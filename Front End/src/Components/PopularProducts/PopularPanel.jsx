e258c07538577523e79e7c242c6e365809221c6d
KavithFernando <153263149+KavithFernando@users.noreply.github.com>
2024-02-20 16:22:35 +0530

    PopularPanel fix

--- Front End/src/Components/PopularProducts/PopularPanel.jsx
+++ Front End/src/Components/PopularProducts/PopularPanel.jsx
@@ -13,15 +13,27 @@ export default function PopularPanel() {
       items: 8
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 3000, min: 1300 },
+      items: 6
+    },
+    middle1: {
+      breakpoint: { max: 1300, min: 1200 },
       items: 5
     },
+    middle2: {
+      breakpoint: { max: 1200, min: 750 },
+      items: 4
+    },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1024, min: 750 },
+      items: 3
+    },
+    middle3: {
+      breakpoint: { max: 750, min: 500 },
       items: 2
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 500, min: 0 },
       items: 1
     }
   };
