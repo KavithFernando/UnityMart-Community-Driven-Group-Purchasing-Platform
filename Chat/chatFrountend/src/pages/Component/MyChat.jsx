81992877210af92e11dcb066fad416b4753837f2
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-02-21 20:14:10 +0530

    chat ui

--- Chat/chatFrountend/src/pages/Component/MyChat.jsx
+++ Chat/chatFrountend/src/pages/Component/MyChat.jsx
@@ -3,10 +3,12 @@ import { ChatState } from "../../Context/chatProvider";
 import { useToast } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect } from "react";
-import { Box } from "@chakra-ui/layout";
+import { Box, Stack, Text } from "@chakra-ui/layout";
 import "./MyChat.css";
 import { Button } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import ChatLoading from "./ChatLoading";
+import { getSender } from "../../config/ChatLogics";
 
 export default function MyChat() {
   const [loggedUser, setLoggedUser] = useState();
@@ -46,29 +48,62 @@ export default function MyChat() {
       <Box
         d={{ base: selectedChat ? "none" : "flex", md: "flex" }}
         className="box1"
-      ></Box>
+      >
+        <Box className="box2">
+          my chat
+          <Button
+            d="flex"
+            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            rightIcon={<AddIcon />}
+          >
+            New Group Chat
+          </Button>
+        </Box>
 
-      <Box className="box2">
-        my chat
-        <Button
+        <Box
           d="flex"
-          fontSize={{ base: "17px", md: "10px", lg: "17px" }}
-          rightIcon={<AddIcon />}
+          flexDir="column"
+          p={3}
+          bg="#F8F8F8"
+          w="100%"
+          h="100%"
+          borderRadius="lg"
+          overflowY="hidden"
         >
-          New Group Chat
-        </Button>
+          {chats ? (
+            <Stack overflowY="scroll">
+              {chats.map((chat) => (
+                <Box
+                  onClick={() => setSelectedChat(chat)}
+                  cursor="pointer"
+                  bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                  color={selectedChat === chat ? "white" : "black"}
+                  px={3}
+                  py={2}
+                  borderRadius="lg"
+                  key={chat._id}
+                >
+                  <Text>
+                    {!chat.isGroupChat
+                      ? getSender(loggedUser, chat.users)
+                      : chat.chatName}
+                  </Text>
+                  {chat.latestMessage && (
+                    <Text fontSize="xs">
+                      <b>{chat.latestMessage.sender.name} : </b>
+                      {chat.latestMessage.content.length > 50
+                        ? chat.latestMessage.content.substring(0, 51) + "..."
+                        : chat.latestMessage.content}
+                    </Text>
+                  )}
+                </Box>
+              ))}
+            </Stack>
+          ) : (
+            <ChatLoading />
+          )}
+        </Box>
       </Box>
-
-      <Box
-        d="flex"
-        flexDir="column"
-        p={3}
-        bg="#F8F8F8"
-        w="100%"
-        h="100%"
-        borderRadius="lg"
-        overflowY="hidden"
-      ></Box>
     </div>
   );
 }
