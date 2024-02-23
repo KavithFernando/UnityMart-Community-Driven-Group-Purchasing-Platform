d06b7036e8861466e606b224e379c3036eaf32e9
Sanjaya Perera <157728209+sanjayaperera@users.noreply.github.com>
2024-02-23 20:45:32 +0530

    some changes

--- Front End/src/Components/buyerproduct.jsx
+++ Front End/src/Components/buyerproduct.jsx
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import ProgressBar from './ProgressBar';
+import './buyerproducr.css';
 
 const BuyerProduct = ({ orderName, orderId, progress }) => {
   return (
     <div className='prop-c1'>
       <div className='prop-c2'>
-        <p>order name: {orderName}</p>
-        <p>order id is: {orderId}</p>
+        <p><p className='p-1'>Order Name:</p> {orderName}</p><br/>
+        <p><p className='p-1'>Order ID is:</p> {orderId}</p><br/>
         <ProgressBar progress={progress} />
         <button>leave queue</button>
       </div>
