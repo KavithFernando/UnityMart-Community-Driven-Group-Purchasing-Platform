db6a54b43d0977219bd2d3e129aa81c093beaa19
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-03-01 16:15:27 +0530

    create server and conect database

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+//import routes
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//route middleware
+
+const Port = 8080;
+const DBurl =
+  "mongodb+srv://pabasara20222053:pabasara20222053@cluster0.miwn7vr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.listen(Port, () => {
+  console.log("Server is start");
+});
+
+mongoose
+  .connect(DBurl)
+
+  .then(() => {
+    console.log("DB conected is sucsess full");
+  })
+  .catch((err) => {
+    console.log("some error " + err);
+  });
