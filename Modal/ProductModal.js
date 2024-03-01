b7eb03a1be019b20d18a7d798f8793b7810adccf
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-03-01 15:48:13 +0530

    Create ProductModal.js

---
+++ Modal/ProductModal.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const productModal = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+
+  brand: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+
+  discountPrice: {
+    type: Number,
+    required: true,
+  },
+
+  Description: {
+    type: String,
+    required: true,
+  },
+
+  Photo: {
+    type: String,
+    required: true,
+  },
+
+  Description: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
