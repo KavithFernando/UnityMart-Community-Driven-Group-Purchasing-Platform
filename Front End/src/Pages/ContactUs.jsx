afd0a0fb66f4b7df3b6c4ad6b8f86230a43e2e44
SubhavithaDharmasiri <150103772+SubhavithaDharmasiri@users.noreply.github.com>
2024-03-03 22:23:27 +0530

    Update ContactUs.jsx

--- Front End/src/Pages/ContactUs.jsx
+++ Front End/src/Pages/ContactUs.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ReactDOM } from 'react-dom';
 import './ContactUs.css'; 
 
-const ContactUs = () => {
-    const [submitted, setSubmitted] = useState(false);
+function App(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState("");
+
     const handleSubmit = (e) => {
         e.preventDefault();
+    };
+    console.log(name,email,message);
+    return(
+        <div className='form'>
+            <form>
+                <div className ='input'>
+                    <lable for = 'name'>Name</lable>
+                    <input type='text' id='name' name='name' placeholder='Your Nanme' value={name}  onChange={(e)=> setName(e.target.value)}/>
+                </div>
+                <div className='input'>
+                    <label for ='email'>E-Mail</label>
+                    <input type='text' id='email' name='email' placeholder='Your E-Mail' value={email} onChange={(e)=> setEmail(e.target.value)} />
+                </div>
+                <div className='input'>
+                    <label for='message'>Message</label>
+                    <textarea name='message' id='message' placeholder='Your Message' value={message}  onChange={(e)=> setMessage(e.target.value)}></textarea>
+                </div>
+                <button type='submit'>Send Message</button>
+            </form>
+        </div>
+    )
+}
 
-        const inputs = e.target.elements;
-        const data = {};
 
-        for (let i = 0; i < inputs.length; i++) {
-            if (inputs[i].name) {
-                data[inputs[i].name] = inputs[i].value;
-            }
-        } 
-    }
-};
+export default ContactUs;
