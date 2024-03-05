21d7b83405bbde3bb63e7213403e5e71d14b64a2
Niketh-Randil <127325580+Niketh-Randil@users.noreply.github.com>
2024-03-05 10:10:10 +0530

    Update API

--- Routes/productorder.js
+++ Routes/productorder.js
@@ -14,3 +14,15 @@ router.post("/Order/save", async (req, res) => {
       return res.status(400).json({ error: err });
     }
   });
+
+
+
+//update the order
+router.put("/Order/update/:id", async (req, res) => {
+    try {
+      await posts.findByIdAndUpdate(req.params.id, { $set: req.body });
+      return res.status(200).json({ success: "post update successful" });
+    } catch (err) {
+      return res.status(400).json({ error: err });
+    }
+  });  
