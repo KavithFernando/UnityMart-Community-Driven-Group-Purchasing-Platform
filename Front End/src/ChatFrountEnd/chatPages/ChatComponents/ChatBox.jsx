2bb51a7c80bbcef06ecb956b522576b02d22f013
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-03-05 15:29:54 +0530

    add main parts in chat

---
+++ Front End/src/ChatFrountEnd/chatPages/ChatComponents/ChatBox.jsx
@@ -0,0 +1,24 @@
+import { Box } from "@chakra-ui/layout";
+import "./ChatBox.css";
+import SingleChat from "./SingleChat";
+import { ChatState } from "../../Context/chatProvider";
+
+const Chatbox = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = ChatState();
+
+  return (
+    <Box
+      className="box1"
+      d={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      p={3}
+      bg="#fa7831"
+      width={{ base: "100%", md: "80%" }}
+      height={"100%"}
+      borderWidth="10px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
+  );
+};
+
+export default Chatbox;
