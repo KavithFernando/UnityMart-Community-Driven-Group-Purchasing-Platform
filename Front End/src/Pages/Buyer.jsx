0dfb23705a19771335bc175f41e47a7f06db8113
Sanjaya Perera <157728209+sanjayaperera@users.noreply.github.com>
2024-03-09 00:29:39 +0530

    salesprediction

--- Front End/src/Pages/Buyer.jsx
+++ Front End/src/Pages/Buyer.jsx
@@ -1,21 +1,33 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import Buyerdetails from "../Components/buyerdetails";
 
 export default function Buyer() {
+  const [predictedSales, setPredictedSales] = useState([]);
+
+  const runMachineLearning = async () => {
+    try {
+      const response = await axios.get("/run-ml");
+      setPredictedSales(response.data.predicted_sales);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   const name = "Pabasara Ravindraka";
   const email = "pabasara12@gmail.com";
   const ordername = "Bluetooth Wireless Headset";
   const orderid = "122";
 
-  //pass the properties
   return (
     <div>
+      <button onClick={runMachineLearning}>sales Prediction</button>
       <Buyerdetails
         name={name}
         email={email}
         props3={ordername}
         props4={orderid}
+        predictedSales={predictedSales} // Pass the predicted sales to the component
       />
     </div>
   );
