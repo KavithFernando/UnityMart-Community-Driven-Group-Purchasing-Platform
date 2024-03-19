0f4a61ddbb811ad63b599295140defa925edbf8d
pabasaraRavindaka <158150970+pabasaraRavindaka@users.noreply.github.com>
2024-03-19 12:10:32 +0530

    loaded seller details in front end

--- Front End/src/Components/sellerproduct.jsx
+++ Front End/src/Components/sellerproduct.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
 
-export default function sellerproduct() {
+export default function Sellerproduct() {
   return (
     <div className="product-card">
       <div className="image-div">
@@ -10,7 +10,12 @@ export default function sellerproduct() {
       <div className="product-card-details">
         <p className="product-card-title">Name</p>
         <p className="product-card-id">Order ID: 123 </p>
-        <p className="product-card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, doloremque consectetur pariatur, vitae quas dolores nam at id nisi facilis totam tempora. Quae cupiditate ea dolorem dignissimos dolorum repellat impedit.</p>
+        <p className="product-card-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum,
+          doloremque consectetur pariatur, vitae quas dolores nam at id nisi
+          facilis totam tempora. Quae cupiditate ea dolorem dignissimos dolorum
+          repellat impedit.
+        </p>
         <div className="product-card-progress">
           <p className="product-card-reach">Reach 100</p>
           <ProgressBar
@@ -27,14 +32,12 @@ export default function sellerproduct() {
         <p className="product-card-more">
           <span>20</span> more to go
         </p>
-        <div className= "buttoncontainer">
-        
-        <div className="product-card-edit">Edit order</div>
-        
-        <div className="product-card-delete">Delete order</div>
-        
+        <div className="buttoncontainer">
+          <div className="product-card-edit">Edit order</div>
+
+          <div className="product-card-delete">Delete order</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
