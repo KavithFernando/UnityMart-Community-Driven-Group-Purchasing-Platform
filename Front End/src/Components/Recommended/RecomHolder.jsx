cc6a15a07a77b3d5e07566489b91871155bf610c
Sanjaya Perera <157728209+sanjayaperera@users.noreply.github.com>
2024-03-19 13:30:13 +0530

    api to get newest

--- Front End/src/Components/Recommended/RecomHolder.jsx
+++ Front End/src/Components/Recommended/RecomHolder.jsx
@@ -3,7 +3,25 @@ import RecomProduct from './RecomProduct'
 import "./RecomHolder.css"
 
 export default function RecomHolder() {
-  return (
+	const [products, setProducts] = useState([]);
+
+  	useEffect(() => {
+    // Define a function to fetch products from the API
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/products/newest');
+        // Assuming the response data is an array of products
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    // Call the fetchProducts function when the component mounts
+    fetchProducts();
+  	}, []); // Empty dependency array ensures this effect runs only once on mount
+	
+  	return (
 		<>
 			<section className='recomSection'>
 				<h2 className='forYou'>More For You</h2>
