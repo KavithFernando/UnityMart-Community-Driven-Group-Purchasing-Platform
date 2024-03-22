33f5d162e489fab7cfcd4016718445a07b10087f
KavithFernando <153263149+KavithFernando@users.noreply.github.com>
2024-03-22 16:34:05 +0530

    Go to Product page on View Details click

--- Front End/src/Components/buyerproduct.jsx
+++ Front End/src/Components/buyerproduct.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function BuyerProduct(props) {
   const [quntity, setQuntity] = useState("");
+  const [sellerInfo, setSellerInfo] = useState({});
   const progress = Math.round((props.current / props.reach) * 100);
+
+  const to_go = props.reach - props.current;
+  const completed = (props.current / props.reach) * 100;
+  const discount = Math.round(((props.storePrice - props.discountPrice) / props.storePrice) * 100);
+  
 
   const getProductQuntaty = async () => {
     try {
@@ -20,9 +27,37 @@ export default function BuyerProduct(props) {
       console.log(err);
     }
   };
+
+  // Function to fetch seller details
+  const fetchSellerInfo = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/user/${props.seller}`);
+      setSellerInfo(response.data);
+    } catch (error) {
+      console.error('Error fetching seller info00:', error);
+    }
+  };
+
   useEffect(() => {
     getProductQuntaty();
+    fetchSellerInfo();
   }, []);
+
+  const handleClick = () => {
+    // Save the product details to local storage
+    localStorage.setItem('productId', props.id);
+    localStorage.setItem('title', props.name);
+    localStorage.setItem('description', props.det)
+    localStorage.setItem('reach', props.reach);
+    localStorage.setItem('price', props.discountPrice);
+    localStorage.setItem('storePrice', props.storePrice);
+    localStorage.setItem('to_go', to_go);
+    localStorage.setItem('completed', completed);
+    localStorage.setItem('discount', discount);
+    localStorage.setItem('sellerName', sellerInfo.name || '');
+    localStorage.setItem('sellerUsername', sellerInfo.userName || '');
+    localStorage.setItem('photo',props.img );
+  };
 
   return (
     <div className="product-card">
@@ -45,13 +80,15 @@ export default function BuyerProduct(props) {
             animateOnRender={true}
           />
 
-          <p> Quntaty: {quntity}</p>
         </div>
 
         <p className="product-card-more">
           <span>{props.reach - props.current}</span> more to go
         </p>
-        <div className="product-card-fullview">View Details</div>
+        <div className="quantity-flexer">
+          <div className="product-card-fullview" onClick={handleClick}><Link to="/Product">View Details</Link></div> 
+          <p className="quantity">Quantity: {quntity}</p>
+        </div>
       </div>
     </div>
   );
