e7bd76c5032edd962fb7657b689fd4f3a945ce38
KavithFernando <153263149+KavithFernando@users.noreply.github.com>
2024-03-23 14:30:18 +0530

    Python code change

--- Recommendation System Python/Search_Engine_Recommendation.py
+++ Recommendation System Python/Search_Engine_Recommendation.py
@@ -1,18 +1,21 @@
 # import numpy as np
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import warnings
 import nltk
 from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
 nltk.download('punkt')
 warnings.filterwarnings('ignore')
 
-# Load the dataset
-amazon_df = pd.read_csv("Dataset/amazon_product_search.csv")
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-# Drop the "id" axis
-amazon_df.drop("id", axis=1, inplace=True)
+# Load the dataset
+amazon_df = pd.read_csv("Dataset/productData.csv")
 
 # Create a SnowballStemmer instance for English
 stemmer = SnowballStemmer("english")
@@ -30,7 +33,7 @@ def tokenize_stem(text):
 
 
 # Apply "tokenize_stem()" to create a new column in a DataFrame
-amazon_df["Stemmed Tokens"] = amazon_df.apply(lambda row: tokenize_stem(row["Title"] + " " + row["Category"]), axis=1)
+amazon_df["Stemmed Tokens"] = amazon_df.apply(lambda row: tokenize_stem(row["productName"] + " " + row["category"]), axis=1)
 
 # Create a TfidfVectorizer instance with the custom tokenizer
 tfidf_vectorizer = TfidfVectorizer(tokenizer=tokenize_stem)
@@ -56,10 +59,19 @@ def search_product(query):
     amazon_df["Similarity"] = amazon_df["Stemmed Tokens"].apply(lambda x: check_cosine_similarity(stemmed_query, x))
 
     # Sort the DataFrame by similarity in descending order and take the top 10 results
-    result = amazon_df.sort_values(by=["Similarity"], ascending=False).head(10)[["Title", "Category"]]
+    result = amazon_df.sort_values(by=["Similarity"], ascending=False).head(10)[["_id"]]
 
     # Return the result containing product titles, descriptions, and categories
-    return result
+    return result["_id"].tolist()
 
 
-print(search_product("Toys"))
+@app.route('/search', methods=['POST'])
+def search():
+    data = request.get_json()
+    query = data['query']
+    result = search_product(query)
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
